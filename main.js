b253bb4dffc2789868abcb1e06df9100a2557b52
Jack Merlin Bruce <j.merlin.b@gmail.com>
2019-05-08 22:04:17 +0100

    added legend

--- main.js
+++ main.js
@@ -24,7 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const yearLabel = document.getElementById('yearLabel')
     const dataLabel = document.getElementById('temperatureLabel')
     const startMessage = document.getElementById('startMessage')
+    const footer = document.getElementById('footer')
+    const gradient = document.getElementById('gradient')
     const body = document.querySelector('body')
+
+    // LEGEND
+    const colour = {
+    	low: '#240F4A',
+    	mid: '#B83856',
+    	high: '#FAC53F'
+    }
+
+    gradient.style.background = `linear-gradient(to right, ${colour.low}, ${colour.mid}, ${colour.high})`
+    // footer.style.background = `linear-gradient(to right, ${colour.low}, ${colour.mid}, ${colour.high})`
 
     // DATA
     const musicScaleNotes = {
@@ -33,6 +45,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     function getScales(data) {
+    	/*
+		This function makes a set of different scales based on any set of 1D data in an array
+    	*/
         let dataMin = Math.min(...data),
             dataMax = Math.max(...data)
 
@@ -73,6 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const data = [-0.19, -0.11, -0.12, -0.2, -0.3, -0.32, -0.33, -0.36, -0.18, -0.11, -0.37, -0.24, -0.27, -0.32, -0.32, -0.23, -0.12, -0.12, -0.28, -0.19, -0.09, -0.16, -0.3, -0.39, -0.49, -0.28, -0.23, -0.4, -0.44, -0.48, -0.43, -0.43, -0.36, -0.36, -0.16, -0.12, -0.33, -0.44, -0.28, -0.27, -0.26, -0.18, -0.27, -0.25, -0.25, -0.21, -0.09, -0.21, -0.19, -0.35, -0.15, -0.1, -0.17, -0.3, -0.14, -0.21, -0.16, -0.04, -0.04, -0.03, 0.11, 0.18, 0.05, 0.07, 0.2, 0.08, -0.07, -0.04, -0.11, -0.11, -0.18, -0.07, 0.01, 0.07, -0.15, -0.14, -0.21, 0.04, 0.07, 0.03, -0.02, 0.05, 0.04, 0.07, -0.2, -0.1, -0.05, -0.02, -0.07, 0.07, 0.03, -0.09, 0.01, 0.16, -0.08, -0.02, -0.11, 0.17, 0.06, 0.16, 0.27, 0.33, 0.13, 0.31, 0.16, 0.12, 0.18, 0.33, 0.41, 0.28, 0.44, 0.41, 0.22, 0.24, 0.31, 0.45, 0.34, 0.47, 0.62, 0.4, 0.4, 0.53, 0.62, 0.61, 0.53, 0.67, 0.62, 0.64, 0.52, 0.63, 0.7, 0.59, 0.62, 0.65, 0.73, 0.87, 0.99, 0.91, 0.83,]
+    // const data = [243024, 344024, 403722, 394206, 546211, 393390, 144285, 93911, 44932, 6361, 10965, 6854, 19320, 24521, 12535, 14586, 24173, 55313, 45493, 75612, 78648, 88993, 186075, 143990, 183814, 189598, 277907, 191929, 227410, 97112, 10411, 10411, 55817, 112928, 48421, 42624, 47524, 67525, 94692, 79266, 74829, 55088, 41587, 9714, 10052, 15503, 17060, 9385, 12600, 12719, 12024, 11496, 12098, 9196, 13436, 6646.25, 7210, 7653.75, 5490, 5294, 9606, 12073, 18899, 25267, 14294, 11760, 12056, 11257, 16583, 21048, 19875,]
     const scales = getScales(data)
     body.style.backgroundColor = scales.colour(data[0])
     labels.style.transform = `scale(0.1)`
